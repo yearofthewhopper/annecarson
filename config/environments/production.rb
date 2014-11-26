391f2b19b1b28ff2b76040947232a3c3a8ae4f00
Noland Chaliha <nolandchaliha@gmail.com>
2014-11-26 12:52:00 -0300

    "fixed foundation production"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 AnneCarson::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.precompile += %w( vendor/modernizr.js )
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +27,8 @@ AnneCarson::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.precompile += %w( vendor/modernizr.js )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
