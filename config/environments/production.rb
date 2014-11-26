1c0a87d1f7530de9f1e1d1093f0d340167219f48
Noland Chaliha <nolandchaliha@gmail.com>
2014-11-26 13:13:52 -0300

    "here another small change "

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ AnneCarson::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.assets.compile = false
-  config.assets.precompile += %w( vendor/modernizr.js )
+  # config.assets.precompile += %w( vendor/modernizr.js )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,7 +44,7 @@ AnneCarson::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
