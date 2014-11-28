2aa1fa5840414a0c32541831166b9d73859007fd
Noland Chaliha <nolandchaliha@gmail.com>
2014-11-28 16:16:35 -0300

    "tricks"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,58 +1,29 @@
 AnneCarson::Application.configure do
- # Code is not reloaded between requests
- config.cache_classes = true
+  # Settings specified here will take precedence over those in config/application.rb.
 
- # Full error reports are disabled and caching is turned on
- config.consider_all_requests_local       = false
- config.action_controller.perform_caching = true
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
 
- # Disable Rails's static asset server (Apache or nginx will already do this)
- config.serve_static_assets = true
+  # Do not eager load code on boot.
+  config.eager_load = false
 
- # Compress JavaScripts and CSS
- config.assets.compress = true
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
- # Don't fallback to assets pipeline if a precompiled asset is missed
- config.assets.compile = true
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
 
- # Generate digests for assets URLs
- config.assets.digest = true
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
 
- # Defaults to nil and saved in location specified by config.assets.prefix
- # config.assets.manifest = YOUR_PATH
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
- # Specifies the header that your server uses for sending files
- # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
- # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
- # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
- # config.force_ssl = true
-
- # See everything in the log (default is :info)
- # config.log_level = :debug
-
- # Prepend all log lines with the following tags
- # config.log_tags = [ :subdomain, :uuid ]
-
- # Use a different logger for distributed setups
- # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
- # Use a different cache store in production
- # config.cache_store = :mem_cache_store
-
- # Enable serving of images, stylesheets, and JavaScripts from an asset server
- # config.action_controller.asset_host = "http://assets.example.com"
-
- # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
- # config.assets.precompile += %w( search.js )
-
- # Disable delivery errors, bad email addresses will be ignored
- # config.action_mailer.raise_delivery_errors = false
-
- # Enable threaded mode
- # config.threadsafe!
-
- # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
- # the I18n.default_locale when a translation can not be found)
- config.i18n.fallbacks = true
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
 end
